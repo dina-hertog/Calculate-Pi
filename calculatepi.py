911f7d714b1575663775620d990ae5863c3bb7da
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-09-16 14:01:31 -0400

    Updated from Brython Server: 9/16/2015 2:01:31 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,6 +1,6 @@
 """
 calculatepi.py
-Author: <your name here>
+Author: Dina
 Credit: <list sources used, if any>
 Assignment:
 
@@ -21,3 +21,9 @@ The approximate value of pi is 3.1315929
 Note: remember that the printed value of pi will be an estimate!
 
 """
+import math
+n = int(input("I will estimate Pi. How many terms should I use? "))
+decimals = int(input("How many decimal places should I use in the result? "))
+Pi = [4*sum((-1**k)/(2*k+1)) for k in range(0,n)]
+print("The approximate value of Pi is {0:.{1}f}".format(Pi, decimals))
+print("(The true value of Pi is {0:.{1}f})".format(math.Pi, decimals))
